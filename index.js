df06fcdb6b3ddfa25d45d570cc2b9314d7b16aac
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-16 00:25:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Code your solution in this file!
 function logDriverNames(drivers) {
-  drivers.map(driver => console.log(driver.name))
+  drivers.map(driver => console.log(driver.name));
 }
 
 function logDriversByHometown(drivers, hometown) {
-  logDriverNames(drivers.filter(driver => driver.hometown === hometown))
+  logDriverNames(drivers.filter(driver => driver.hometown === hometown));
 }
 
 function driversByRevenue(drivers) {
-  drivers.sort(revenue);
+  drivers.sort(driver.revenue);
 }
